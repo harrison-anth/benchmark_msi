c7c0b94ec0786fa72ab1c72f9fed830fedac9f8a
Harrison Anthony <harrison.anthony@wsu.edu>
2024-06-25 00:08:09 +0100

    begin toy example finalization

--- toy_example/scripts/msingb.sh
+++ toy_example/scripts/msingb.sh
@@ -1,7 +1,24 @@
+#activate conda environment
+eval "$(conda shell.bash hook)"
 conda activate msingb
 
 maf=$1
 
-python ../reference_files/MSINGB/codes/msingb.py --maf $maf --outdir output/msingb_test/
+echo "Running MSINGB"
 
+python ../reference_files/MSINGB/codes/msingb.py --maf $maf --outdir output/
 
+if [[ -f output/preres.csv ]]
+then
+echo "MSINGB ran successfully."
+echo "see output/preres.csv for details."
+fi
+
+if [[ ! -f output/preres.csv ]]
+then
+echo "MSINGB failed. Check program output for error messages."
+fi
+
+#wait 4 seconds for user to see success/fail outcome.
+
+sleep 4
